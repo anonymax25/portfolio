04d1e6b99f43e767d428f0d98f0d47e993939686
anonymax25 <natin56y@gmail.com>
2025-10-01 15:56:49 +1000

    fix: ci

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({mode}) => ({
   plugins: [react()],
-  base:  mode === "development" ? "/portfolio" : "/portfolio/",
 }))
