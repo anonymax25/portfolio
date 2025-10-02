90870fb560931a7b4cfe2d7625eac87e6770f0fe
anonymax25 <natin56y@gmail.com>
2025-10-02 11:23:04 +1000

    chore: migrate tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindcss from "@tailwindcss/vite";
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
@@ -33,5 +34,7 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    tailwindcss()
+  ],
 };
